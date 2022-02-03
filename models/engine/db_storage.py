7aec8c44c43cd015cc849f378e5b385926c3a173
PaolaAndreaGA <paga0209@gmial.com>
2022-02-03 18:45:18 -0500

    add get method

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,14 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """Returns object based on the class & its ID, or None if not found"""
+    for objects in self.all(cls).values():
+        if objects.id == id:
+            return(objects)
+    return(None)
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        return(len(self.all(cls)))
