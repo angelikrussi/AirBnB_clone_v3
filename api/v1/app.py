1d5878fdd4237b1e58adbe0a79d4aae7a4db73f9
PaolaAndreaGA <paga0209@gmial.com>
2022-02-05 00:40:07 -0500

    UPDATE change documentation app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" app.py """
+""" Connect to API """
+import os
 from models import storage
 from api.v1.views import app_views
-from os import environ
-from flask import Flask, render_template, make_response, jsonify
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
 
