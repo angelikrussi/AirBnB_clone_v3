369bbabd17a0891f57e31cc99ed1036353a3124a
angelikrussi <hannamrrussi@gmail.com>
2022-02-04 23:39:35 -0500

    index -v1

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,7 @@ from flask import Blueprint
 from api.v1.views.index import *
 from flask import Flask, Blueprint, jsonify
 
+
 @app_views.route('/status', strict_slashes=False)
 def hbnbStatus():
     """hbnbStatus"""
