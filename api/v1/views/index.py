d2bbacef2a7a8efe00aca2899b3e74d9eaad3c1e
PaolaAndreaGA <paga0209@gmial.com>
2022-02-05 00:32:03 -0500

    UPDATE fix pep8 index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """Index"""
-from models import storage
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
+from models import storage
 
 
 @app_views.route('/status', strict_slashes=False)
